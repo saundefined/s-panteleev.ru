6f6dce81e7c5404767ffd75068b50f7974da66e7
Sergey Panteleev <sergey@php.net>
2021-09-21 09:36:46 +0300

    feat(schema): Разметка статьи

---
+++ source/_components/schema-org-publisher.blade.php
@@ -0,0 +1,6 @@
+<div itemprop="publisher" itemscope itemtype="https://schema.org/Person">
+    <link itemprop="url" href="{{ $page->baseUrl }}">
+    <meta itemprop="name" content="{{ $page->siteName }}">
+    <meta itemprop="description" content="{{ $page->siteDescription }}">
+    <meta itemprop="email" content="{{ $page->email }}">
+</div>
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -11,7 +11,17 @@
         <link rel="alternate" hreflang="en" href="{{ $original['url'] }}"/>
     @endif
     @if ($page->production)
-    <!-- Insert analytics code here -->
+    <!-- Yandex.Metrika counter -->
+        <script type="text/javascript"> (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {(m[i].a = m[i].a || []).push(arguments);};
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                k, a);
+          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+          ym(50044507, 'init', {clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true}); </script>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/50044507" style="position:absolute; left:-9999px;" alt=""/></div>
+        </noscript> <!-- /Yandex.Metrika counter -->
     @endif
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 </head>
